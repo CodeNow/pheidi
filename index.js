0b824d480ed299434fb94be7a1a27d1db360b957
Anton Podviaznikov <podviaznikov@gmail.com>
2016-04-19 16:20:29 -0700

    create addiotnal rabbit client and worker server

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const CriticalError = require('error-cat/errors/critical-error')
 const ErrorCat = require('error-cat')
 const log = require('logger').child({ module: 'main' })
 const server = require('worker-server')
+const queueServer = require('queue-worker-server')
 
-server.start()
+queueServer.start()
   .then(() => {
-    log.info('Pheidi Worker Server Started')
+    server.start()
+      .then(() => {
+        log.info('Pheidi Worker Server Started')
+      })
+      .catch((err) => {
+        log.fatal({ err: err }, 'Worker server failed to start')
+        ErrorCat.report(new CriticalError(
+          'Pheidi Worker Server Failed to Start',
+          { err: err }
+        ))
+        process.exit(1)
+      })
   })
   .catch((err) => {
-    log.fatal({ err: err }, 'Worker server failed to start')
+    log.fatal({ err: err }, 'Worker queue server failed to start')
     ErrorCat.report(new CriticalError(
-      'Pheidi Worker Server Failed to Start',
+      'Pheidi Queue Worker Server Failed to Start',
       { err: err }
     ))
     process.exit(1)
