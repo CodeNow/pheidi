fb8f23a881bc9b89ffb5c3722bae21c0f7092da5
Anton Podviaznikov <podviaznikov@gmail.com>
2016-04-19 18:39:09 -0700

    change start pheidi order

--- index.js
+++ index.js
@@ -2,32 +2,27 @@
 
 require('loadenv')()
 
+const Promise = require('bluebird')
 const CriticalError = require('error-cat/errors/critical-error')
 const ErrorCat = require('error-cat')
 const log = require('logger').child({ module: 'main' })
 const server = require('worker-server')
 const queueServer = require('queue-worker-server')
+const rabbitmq = require('rabbitmq')
 
-queueServer.start()
-  .then(() => {
-    server.start()
-      .then(() => {
-        log.info('Pheidi Worker Server Started')
-      })
-      .catch((err) => {
-        log.fatal({ err: err }, 'Worker server failed to start')
-        ErrorCat.report(new CriticalError(
-          'Pheidi Worker Server Failed to Start',
-          { err: err }
-        ))
-        process.exit(1)
-      })
-  })
-  .catch((err) => {
-    log.fatal({ err: err }, 'Worker queue server failed to start')
-    ErrorCat.report(new CriticalError(
-      'Pheidi Queue Worker Server Failed to Start',
-      { err: err }
-    ))
-    process.exit(1)
-  })
+Promise.all([
+  queueServer.start,
+  server.start,
+  rabbitmq.publisher.connectAsync
+])
+.then(() => {
+  log.info('Pheidi Server Started')
+})
+.catch((err) => {
+  log.fatal({ err: err }, 'Server failed to start')
+  ErrorCat.report(new CriticalError(
+    'Pheidi Worker Server Failed to Start',
+    { err: err }
+  ))
+  process.exit(1)
+})
