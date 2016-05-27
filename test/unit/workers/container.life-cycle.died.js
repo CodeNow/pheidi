b7763be4bbd4cefc1c5657704acd15ceec72d21e
Myztiq <kahn@runnable.com>
2016-05-27 14:43:41 -0700

    Refactored mongoDbHelper to make it possible to stub properly

---
+++ test/unit/workers/container.life-cycle.died.js
@@ -0,0 +1,62 @@
+/**
+ * @module unit/workers/instance.deleted
+ */
+'use strict'
+
+const chai = require('chai')
+const Promise = require('bluebird')
+const sinon = require('sinon')
+const mongodbHelper = require('mongo-helper')
+const TaskFatalError = require('ponos').TaskFatalError
+
+require('sinon-as-promised')(Promise)
+chai.use(require('chai-as-promised'))
+const assert = chai.assert
+
+const Worker = require('workers/container.life-cycle.died')
+
+describe('Container life-cycle died Worker', () => {
+  const testInstance = {
+    _id: '1234',
+    isTesting: true
+  }
+  const testParams = {
+    id: 'fakeId',
+    inspectData: {
+      Config: {
+        Labels: {
+          type: 'image-builder-container',
+          instanceId: '1234'
+        }
+      }
+    }
+  }
+  let mongoHelperStubs
+  beforeEach((done) => {
+    mongoHelperStubs = {
+      findOneInstanceAsync: sinon.stub().resolves(testInstance)
+    }
+    sinon.stub(mongodbHelper, 'helper').returns(mongoHelperStubs)
+    done()
+  })
+
+  afterEach((done) => {
+    mongodbHelper.helper.restore()
+    done()
+  })
+
+  it('should fail if the instance is not for testing', (done) => {
+    mongoHelperStubs.findOneInstanceAsync.resolves({
+      _id: '1234',
+      isTesting: false
+    })
+    Worker(testParams).asCallback(function (err) {
+      assert.isDefined(err)
+      assert.instanceOf(err, TaskFatalError)
+      assert.match(err.message, /not for testing/i)
+      assert(err.report).equals(false)
+      done()
+    })
+    done()
+  })
+})
