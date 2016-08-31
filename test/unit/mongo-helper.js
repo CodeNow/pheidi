fd70a959fd31a788281f179b5a8c1c710d1d3a86
thejsj <jorge.silva@thejsj.com>
2016-08-31 11:49:03 -0700

    Switch function to MongoHelper

--- test/unit/mongo-helper.js
+++ test/unit/mongo-helper.js
@@ -14,59 +14,122 @@ var sinon = require('sinon')
 var MongoDB = require('models/mongo')
 
 // internal (being tested)
-var mongodbHelper = require('mongo-helper').helper
+var mongodbHelper = require('mongo-helper')
 
 describe('MongoDB Helper', function () {
-  beforeEach(function () {
-    sinon.stub(MongoDB.prototype, 'close').yieldsAsync()
-    sinon.stub(MongoDB.prototype, 'connect').yieldsAsync()
-  })
-  afterEach(function () {
-    MongoDB.prototype.close.restore()
-    MongoDB.prototype.connect.restore()
+  describe('#helper', () => {
+    beforeEach(function () {
+      sinon.stub(MongoDB.prototype, 'close').yieldsAsync()
+      sinon.stub(MongoDB.prototype, 'connect').yieldsAsync()
+    })
+    afterEach(function () {
+      MongoDB.prototype.close.restore()
+      MongoDB.prototype.connect.restore()
+    })
+
+    it('should return a client for Promise.using', function () {
+      var mongodbPromise = mongodbHelper.helper()
+      return assert.isFulfilled(
+        Promise.using(mongodbPromise, function (client) {
+          assert.ok(client)
+          assert.instanceOf(client, MongoDB)
+          sinon.assert.calledOnce(MongoDB.prototype.connect)
+          sinon.assert.notCalled(MongoDB.prototype.close)
+        })
+      )
+    })
+
+    it('should close the client if it was being used', function () {
+      var mongodbPromise = mongodbHelper.helper()
+      return assert.isRejected(
+        Promise.using(mongodbPromise, function (client) {
+          assert.ok(client)
+          assert.instanceOf(client, MongoDB)
+          sinon.assert.calledOnce(MongoDB.prototype.connect)
+          throw new Error('innerError')
+        }),
+        Error,
+        'innerError'
+      )
+        .then(function () {
+          sinon.assert.calledOnce(MongoDB.prototype.close)
+        })
+    })
+
+    it('should log an error if the client fails to close, but return true', function () {
+      MongoDB.prototype.close.yieldsAsync(new Error('foobar'))
+      var mongodbPromise = mongodbHelper.helper()
+      return assert.isFulfilled(
+        Promise.using(mongodbPromise, function () {
+          return 1
+        })
+      )
+        .then(function (result) {
+          // we don't actually get to see the error happen, and that's the point
+          sinon.assert.calledOnce(MongoDB.prototype.close)
+          assert.equal(result, 1)
+        })
+    })
   })
 
-  it('should return a client for Promise.using', function () {
-    var mongodbPromise = mongodbHelper()
-    return assert.isFulfilled(
-      Promise.using(mongodbPromise, function (client) {
-        assert.ok(client)
-        assert.instanceOf(client, MongoDB)
-        sinon.assert.calledOnce(MongoDB.prototype.connect)
-        sinon.assert.notCalled(MongoDB.prototype.close)
-      })
-    )
-  })
+  describe.only('#getUserEmailByGithubId', () => {
+    const githubId = 1981198
+    const userEmail = 'jorge@runnable.com'
+    const user = {
+      email: userEmail
+    }
+    beforeEach(function () {
+      sinon.stub(mongodbHelper, 'helper').resolves(new MongoDB())
+      sinon.stub(MongoDB.prototype, 'findOneUserAsync').resolves(user)
+    })
+    afterEach(function () {
+      mongodbHelper.helper.restore()
+      MongoDB.prototype.findOneUserAsync.restore()
+    })
 
-  it('should close the client if it was being used', function () {
-    var mongodbPromise = mongodbHelper()
-    return assert.isRejected(
-      Promise.using(mongodbPromise, function (client) {
-        assert.ok(client)
-        assert.instanceOf(client, MongoDB)
-        sinon.assert.calledOnce(MongoDB.prototype.connect)
-        throw new Error('innerError')
-      }),
-      Error,
-      'innerError'
-    )
-      .then(function () {
-        sinon.assert.calledOnce(MongoDB.prototype.close)
-      })
-  })
+    it('should connect to the helper', (done) => {
+      mongodbHelper.getUserEmailByGithubId(githubId)
+        .then(() => {
+          sinon.assert.calledOnce(mongodbHelper.helper)
+        })
+        .asCallback(done)
+    })
 
-  it('should log an error if the client fails to close, but return true', function () {
-    MongoDB.prototype.close.yieldsAsync(new Error('foobar'))
-    var mongodbPromise = mongodbHelper()
-    return assert.isFulfilled(
-      Promise.using(mongodbPromise, function () {
-        return 1
-      })
-    )
-      .then(function (result) {
-        // we don't actually get to see the error happen, and that's the point
-        sinon.assert.calledOnce(MongoDB.prototype.close)
-        assert.equal(result, 1)
-      })
+    it('should fint the user by its githubid', (done) => {
+      mongodbHelper.getUserEmailByGithubId(githubId)
+        .then(() => {
+          sinon.assert.calledOnce(MongoDB.prototype.findOneUserAsync)
+          sinon.assert.calledWithExactly(MongoDB.prototype.findOneUserAsync, { 'accounts.github.id': githubId })
+        })
+        .asCallback(done)
+    })
+
+    it('should return the user', (done) => {
+      mongodbHelper.getUserEmailByGithubId(githubId)
+        .then((returnedUserEmail) => {
+          assert.equal(returnedUserEmail, userEmail)
+        })
+        .asCallback(done)
+    })
+
+    it('should return `null` if there is no user', (done) => {
+      MongoDB.prototype.findOneUserAsync.resolves(undefined)
+
+      mongodbHelper.getUserEmailByGithubId(githubId)
+        .then((returnedUserEmail) => {
+          assert.equal(returnedUserEmail, null)
+        })
+        .asCallback(done)
+    })
+
+    it('should return `null` if there is no user email', (done) => {
+      MongoDB.prototype.findOneUserAsync.resolves({ email: undefined })
+
+      mongodbHelper.getUserEmailByGithubId(githubId)
+        .then((returnedUserEmail) => {
+          assert.equal(returnedUserEmail, null)
+        })
+        .asCallback(done)
+    })
   })
 })
---
+++ test/unit/workers/organization.payment-method.added.js
@@ -0,0 +1,101 @@
+'use strict'
+
+const chai = require('chai')
+const SendGrid = require('models/sendgrid')
+const Promise = require('bluebird')
+const sinon = require('sinon')
+const WorkerStopError = require('error-cat/errors/worker-stop-error')
+
+require('sinon-as-promised')(Promise)
+chai.use(require('chai-as-promised'))
+const assert = chai.assert
+const NotifyPaymentMethodAdded = require('workers/organization.payment-method.added').task
+const mongoHelper = require('mongo-helper')
+
+describe('organization.payment-method.added', () => {
+  describe('Worker', () => {
+    let validJob
+    const organizationName = 'helloWorld'
+    const userGithubId = 1981198
+    const paymentMethodOwnerEmail = 'jorge@runnable.com'
+
+    beforeEach(() => {
+      validJob = {
+        organization: {
+          name: organizationName
+        },
+        paymentMethodOwner: {
+          githubId: userGithubId
+        }
+      }
+      sinon.stub(mongoHelper, 'getUserEmailByGithubId').resolves(paymentMethodOwnerEmail)
+      sinon.stub(SendGrid.prototype, 'paymentMethodAdded').resolves(true)
+    })
+
+    afterEach(() => {
+      SendGrid.prototype.paymentMethodAdded.restore()
+      mongoHelper.getUserEmailByGithubId.restore()
+    })
+
+    it('should get the email for the user', (done) => {
+      NotifyPaymentMethodAdded(validJob)
+        .then(() => {
+          sinon.assert.calledOnce(mongoHelper.getUserEmailByGithubId)
+          sinon.assert.calledWithExactly(mongoHelper.getUserEmailByGithubId,
+            userGithubId
+          )
+        })
+        .asCallback(done)
+    })
+
+    it('should send the email', (done) => {
+      NotifyPaymentMethodAdded(validJob)
+        .then(() => {
+          sinon.assert.calledOnce(SendGrid.prototype.paymentMethodAdded)
+          sinon.assert.calledWithExactly(
+            SendGrid.prototype.paymentMethodAdded,
+            organizationName,
+            paymentMethodOwnerEmail
+          )
+        })
+        .asCallback(done)
+    })
+
+    it('should throw any `WorkerStopError` if no email is returned', (done) => {
+      mongoHelper.getUserEmailByGithubId.resolves(null)
+
+      NotifyPaymentMethodAdded(validJob)
+        .asCallback((err) => {
+          assert.isOk(err)
+          assert.instanceOf(err, WorkerStopError)
+          assert.match(err.message, /payment.*method.*owner.*email/i)
+          done()
+        })
+    })
+
+    it('should throw any error thrown by `getUserEmailByGithubId`', (done) => {
+      const thrownErr = new Error()
+      mongoHelper.getUserEmailByGithubId.rejects(thrownErr)
+
+      NotifyPaymentMethodAdded(validJob)
+        .asCallback((err) => {
+          assert.isOk(err)
+          assert.equal(err, thrownErr)
+          done()
+        })
+    })
+
+    it('should throw a `WorkerStopError` if it cant send the email', (done) => {
+      const thrownErr = new Error()
+      SendGrid.prototype.paymentMethodAdded.rejects(thrownErr)
+
+      NotifyPaymentMethodAdded(validJob)
+        .asCallback((err) => {
+          assert.isOk(err)
+          assert.instanceOf(err, WorkerStopError)
+          assert.match(err.message, /failed.*email/i)
+          done()
+        })
+    })
+  })
+})
